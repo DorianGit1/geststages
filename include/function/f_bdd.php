2dd21a9807fa0c1da8082c7bc2e9f99784961bc8
DorianGit1 <dgastaud.ext@simplonformations.onmicrosoft.com>
2024-06-13 15:58:48 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -6,25 +6,19 @@ function connexionBDD()
     {
         $options = array(
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_SSL_CA => '<?php
-
-function connexionBDD()
-{
-    try
-    {
-        $options = array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem',
+            PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem' // Chemin vers le certificat RootCA de Azure
         );
 
-        $bdd = new PDO('mysql:host=doriangeststages-server.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 
-                       'rjkjpaplkx', 
-                       'hk2$Slp$S4D2fXsM', 
-                       $options);
+        $bdd = new PDO(
+            'mysql:host=doriangeststages-server.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 
+            'rjkjpaplkx', 
+            'hk2$Slp$S4D2fXsM', 
+            $options
+        );
 
         return $bdd;
     }
-    catch(Exception $e)
+    catch (Exception $e)
     {
         $pdo_error = $e->getMessage();
         return false;
@@ -32,21 +26,4 @@ function connexionBDD()
 }
 
 ?>
-', // Exemple de chemin vers le certificat RootCA
-        );
 
-        $bdd = new PDO('mysql:host=doriangeststages-server.mysql.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 
-                       'rjkjpaplkx', 
-                       'hk2$Slp$S4D2fXsM', 
-                       $options);
-
-        return $bdd;
-    }
-    catch(Exception $e)
-    {
-        $pdo_error = $e->getMessage();
-        return false;
-    }
-}
-
-?>
